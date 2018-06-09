131400ae03d9da914e1637df09ef81639b383195
Ключников Евгений Владимирович <EKlyuchnikov@ibs.root.ibscorp.ru>
2018-06-09 17:29:26 +0300

    bootstrap v4.1.1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PageNavigatorComponent } from './page-navigator/page-navigator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNavigatorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/page-navigator/page-navigator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-navigator',
+  templateUrl: './page-navigator.component.html',
+  styleUrls: ['./page-navigator.component.css']
+})
+export class PageNavigatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
