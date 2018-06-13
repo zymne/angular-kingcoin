01d9c7f9e3b6025e32c2e3ef2c26e212063b3dee
Ключников Евгений Владимирович <EKlyuchnikov@ibs.root.ibscorp.ru>
2018-06-13 19:01:07 +0300

    -

--- src/app/page-navigator/page-navigator.component.ts
+++ src/app/page-navigator/page-navigator.component.ts
@@ -9,12 +9,33 @@ export class PageNavigatorComponent implements OnInit {
   
   //pageControl = [1, 2, 3 , 4]
   
+  visiblePages = 8;
+  firstPage = 1;
+  lastPage = this.visiblePages;
+  
+  
+  firstVisiblePages = 5;
+  lastVisiblePages = 2;
+
   get pageControl() {
-    return [1, 2, 3 , 4, 5];
+    return [1, 2, 3 , 4, 5, 6, 7, 8, 9, 10, 11];
+  }
+
+  get pagesNumber() {
+    return this.pageControl.length;
   }
 
   currentPage = 1;
 
+  scrollLeft(curr) {
+    
+    //round - in case if num pages !odd
+    var middlePage = (this.lastPage - this.firstPage)/2;
+    
+
+    ++this.firstPage
+  }
+  
   onClick($event, pg) {    
     this.currentPage = pg;
   }
@@ -26,6 +47,16 @@ export class PageNavigatorComponent implements OnInit {
     else
       return false;
 
+  }
+
+  goNext() {    
+    if(this.pagesNumber > this.currentPage)
+      ++this.currentPage;
+  }
+
+  goPrev() {
+    if(this.currentPage > 1)
+      --this.currentPage;
   }
 
   constructor() { }
