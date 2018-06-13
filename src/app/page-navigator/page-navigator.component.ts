b8d575ef6fb7a36733bd5a636fa028234a2fec7e
Ключников Евгений Владимирович <EKlyuchnikov@ibs.root.ibscorp.ru>
2018-06-13 13:27:22 +0300

    -

--- src/app/page-navigator/page-navigator.component.ts
+++ src/app/page-navigator/page-navigator.component.ts
@@ -6,8 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-navigator.component.css']
 })
 export class PageNavigatorComponent implements OnInit {
+  
+  //pageControl = [1, 2, 3 , 4]
+  
+  get pageControl() {
+    return [1, 2, 3 , 4, 5];
+  }
 
-  heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  currentPage = 1;
+
+  onClick($event, pg) {    
+    this.currentPage = pg;
+  }
+
+  isPageActive(pg) {
+    if(pg == this.currentPage) {
+      return true;
+    }
+    else
+      return false;
+
+  }
 
   constructor() { }
 
